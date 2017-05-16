fee0b10aacc5fe4057e40ee8064d5c147f50d9a9
jamiecjm <jamiecjm@gmail.com>
2017-05-16 18:32:53 +0800

    create

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Lineup
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.sass.load_paths << File.expand_path('../../lib/assets/stylesheets/')
+    config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
   end
 end
