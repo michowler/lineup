826fef143e1c215f40c1f730de014e826887fdd4
sofiaadam <sofiaadamkho@gmail.com>
2017-05-17 12:36:31 +0800

    login api working

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,14 @@ Rails.application.routes.draw do
     resources :leaves
   end
 
+  #api for mobile
+  namespace :api do
+    namespace :v1 do
+      resources :sessions, only: [:create]
+    end
+  end
+  
+
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
