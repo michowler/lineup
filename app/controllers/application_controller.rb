1bee17bdeffc61b795916b730d07d8e2fad359d8
jamiecjm <jamiecjm@gmail.com>
2017-05-17 12:03:03 +0800

    restrict access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery with: :exception
+
+  if respond_to?(:before_action)
+    before_action :login_required
+  else
+    before_filter :login_required
+  end
+
+  def login_required
+  	permitted_path = ["/passwords/new","/sign_in","/passwords","/session"]
+  	if !signed_in? 
+  		redirect_to sign_in_path unless permitted_path.include?(request.fullpath)
+  	end
+  end
+
 end
