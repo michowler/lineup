fddda6d38f7aa0b7c929304a45b4a2d9f653ff14
sofiaadam <sofiaadamkho@gmail.com>
2017-05-18 13:40:13 +0800

    created leaves api

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :sessions, only: [:create]
       resources :users, only: [:index, :show]
+      resources :leaves, only: [:create]
     end
   end
   
