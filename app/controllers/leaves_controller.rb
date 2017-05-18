80609f81c832760aaa4ef7dc65f0603972d805f9
jamiecjm <jamiecjm@gmail.com>
2017-05-18 18:03:38 +0800

    added calendar and adjust position

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -5,7 +5,7 @@ class LeavesController < ApplicationController
 	end
 
 	def pending
-		@leaves = Leafe.where(status: "Pending").includes(:user)
+		@leaves = Leafe.where(status: "Pending").where("start_date > ?",Time.now).order("start_date").includes(:user)
 	end
 
 	def show
