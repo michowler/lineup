4d5c9747a82e4de60848ce19b0dc9839b5cfdf50
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-05-18 18:05:51 +0800

    edit index css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -54,3 +54,11 @@ function yesterday(date){
 	return $yyyy+'-'+$mm+'-'+$dd;
 }
 
+$(window).scroll(function () {
+	if ($(window).scrollTop() >= 100) {
+	$('.navbar').css('background','rgba(235,235,235,0.5)');
+	} 
+	else {
+	$('.navbar').css('background','transparent');
+	}
+});
