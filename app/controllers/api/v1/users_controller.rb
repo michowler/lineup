b863634c79b8d20bef6b0153f609dec3b6292475
sofiaadam <sofiaadamkho@gmail.com>
2017-05-20 19:38:43 +0800

    show user leaves done

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,23 +21,29 @@ class Api::V1::UsersController < Api::V1::ApplicationController
     #NEED TO DO manager name
     def show
         user = User.where(private_token: params[:private_token]).first
+        user_leave = user
+        user_leave = user_leave.id
+		user_leave = User.find(user_leave).leaves 
+        user_leave = user_leave.as_json(only: [:leave_type, :start_date, :end_date, :total_days, :status])
         # manager = User.where(manager_id: 1)
         # user = user.to_json
-        # respond_to do |format|
-        #     format.json { render :json => {:user => user,
-        #     :manager_name => manager_name}}
-        render json: user, status: :ok
-        # end
+        respond_to do |format|
+            format.json { render :json => {:user => user,
+            :user_leave => user_leave}}
+        # render json: user, status: :ok
+        end
     end
 
-    def user_leaves
-        # user = User.where(private_token: params[:private_token])
-		user = User.find(params[:id]).leaves
-        # user = user.leaves
-        user = user.pluck(:leave_type, :start_date, :end_date, :total_days)
-        return user
-        render json: user, status: :ok
-    end
+    # def user_leaves
+    #     user_leave = User.find(private_token: params[:private_token])
+	# 	user_leave = User.find(params[:id]).leaves
+    #     # user = user.leaves
+    #     user_leave = user.pluck(:leave_type, :start_date, :end_date, :total_days).to_s
+    #     return user
+    #     render json: user_leave, status: :ok
+    # end
     
 end
+
+
 
