0d4c3618961b578f0d3dea4dc8660cfd1f2b7032
sofiaadam <sofiaadamkho@gmail.com>
2017-05-21 12:11:12 +0800

    code for total leaves api, haven't tested

--- app/controllers/api/v1/total_leaves_controller.rb
+++ app/controllers/api/v1/total_leaves_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::TotalLeavesController < Api::V1::ApplicationController
+
+    def total_leaves
+        user = User.where(private_token: params[:private_token]).first
+        find_leave = user.id
+        # find_leave = find_leave.id
+		find_leave = User.find(find_leave).leaves 
+        annual = find_leave.where(leave_type: "0", status: "Approved").sum(:total_days).to_s
+        maternity = find_leave.where(leave_type: "1", status: "Approved").sum(:total_days).to_s
+        emergency = find_leave.where(leave_type: "2", status: "Approved").sum(:total_days).to_s
+        study = find_leave.where(leave_type: "3", status: "Approved").sum(:total_days).to_s
+        sick = find_leave.where(leave_type: "4", status: "Approved").sum(:total_days).to_s
+        non_paid = find_leave.where(leave_type: "5", status: "Approved").sum(:total_days).to_s
+        respond_to do |format|
+            format.json { render :json => {:annual => annual, :maternity => maternity, :emergency => emergency, :study = > study, :sick => sick, :non_paid = non_paid}}
+        end
+    end
+    
 end
