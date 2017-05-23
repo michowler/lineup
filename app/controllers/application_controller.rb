d05f3a974b85d6af21765da4cf99edb7d50cd7e5
jamiecjm <jamiecjm@gmail.com>
2017-05-23 11:50:21 +0800

    add edit profile button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   if respond_to?(:before_action)
     before_action :login_required
